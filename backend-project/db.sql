a341ff19dc7f08e151c3e171106baabf501c2913
01432468 <lizhitao@sf-express.com>
2024-05-20 20:35:37 +0800

    feat: init project ✈

---
+++ backend-project/db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE meeting_room_booking_system DEFAULT CHARACTER SET utf8mb4;
---
+++ backend-project/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { Permission } from './user/entities/permission.entity';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      // 数据库模块
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'meeting_room_booking_system',
+      entities: [User, Role, Permission],
+      synchronize: true,
+      logging: true,
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
